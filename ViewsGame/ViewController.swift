02c49083e1177a8784752ee5930838312b7d0dcd
IsaikinSergei <akinfest007@gmail.com>
2020-12-28 13:45:25 +0400

    Initial Commit

---
+++ ViewsGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewsGame
+//
+//  Created by Sergei Isaikin on 28.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
